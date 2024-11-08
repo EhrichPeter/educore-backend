3659c6d6a92011306eedc0391514381aa2c1e5df
Peter Ehrich <pe.ehrich@gmail.com>
2024-11-08 17:36:10 +0100

    Add simple upload endpoint

--- src/ingestion/ingestion.controller.ts
+++ src/ingestion/ingestion.controller.ts
@@ -1,4 +1,43 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  FileTypeValidator,
+  MaxFileSizeValidator,
+  ParseFilePipe,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { parse } from 'csv-parse';
 
+function parseCsv(buffer: Buffer): Promise<any[]> {
+  return new Promise((resolve, reject) => {
+    parse(buffer, { columns: true }, (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(data);
+      }
+    });
+  });
+}
 @Controller('ingestion')
-export class IngestionController {}
+export class IngestionController {
+  @Post('csv')
+  @UseInterceptors(FileInterceptor('csvFile'))
+  async uploadCsv(
+    @UploadedFile(
+      new ParseFilePipe({
+        validators: [
+          new MaxFileSizeValidator({ maxSize: 20000 }),
+          new FileTypeValidator({ fileType: 'text/csv' }),
+        ],
+      }),
+    )
+    file: Express.Multer.File,
+  ) {
+    console.log(file);
+    const parsedFile = await parseCsv(file.buffer);
+    console.log(parsedFile);
+  }
+}
